04c9365bbcc40cd34385072d41a54b518f69196a
Abhishek Nishad <abhishek.nishad@celtic.bz>
2023-07-19 17:22:20 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { useGetAllPostQuery } from './services/post';
+
+function App() {
+  const responseInfo = useGetAllPostQuery();
+
+  console.log("response Info : ", responseInfo);
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
